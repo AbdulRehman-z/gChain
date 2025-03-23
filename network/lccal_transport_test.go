e48f7249745d44bd49ff0415c443f89b48caf592
AbdulRehman-z <yousafbhaikhan10@gmail.com>
2025-03-23 15:58:45 +0500

    fix: update peer address accessors in transport tests

--- network/lccal_transport_test.go
+++ network/lccal_transport_test.go
@@ -13,8 +13,8 @@ func TestConnect(t *testing.T) {
 	tra.Connect(trb)
 	trb.Connect(tra)
 
-	assert.Equal(t, tra.peers[trb.addr], trb)
-	assert.Equal(t, trb.peers[tra.addr], tra)
+	assert.Equal(t, tra.peers[trb.Addr()], trb)
+	assert.Equal(t, trb.peers[tra.Addr()], tra)
 }
 
 func TestConsume(t *testing.T) {
@@ -24,8 +24,8 @@ func TestConsume(t *testing.T) {
 	tra.Connect(trb)
 	trb.Connect(tra)
 
-	assert.Equal(t, tra.peers[trb.addr], trb)
-	assert.Equal(t, trb.peers[tra.addr], tra)
+	assert.Equal(t, tra.peers[trb.Addr()], trb)
+	assert.Equal(t, trb.peers[tra.Addr()], tra)
 
 	msg := []byte("Hello, World!")
 	assert.Nil(t, tra.SendMessage(trb.Addr(), msg), msg)
