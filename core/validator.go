19bf938d7e6fe83954d156c07ea2050649da130d
AbdulRehman-z <yousafbhaikhan10@gmail.com>
2025-03-26 18:14:26 +0500

    fix: enhance block validation to check previous block height and hash

--- core/validator.go
+++ core/validator.go
@@ -19,6 +19,20 @@ func (bv *BlockValidator) ValidateBlock(b *Block) error {
 		return fmt.Errorf("block %d already exists", b.Height)
 	}
 
+	if b.Height != bv.bc.Height()+1 {
+		return fmt.Errorf("height %d is greater than blockchain height %d", b.Height, bv.bc.Height())
+	}
+
+	prevHeader, err := bv.bc.GetHeader(b.Height - 1)
+	if err != nil {
+		return err
+	}
+
+	hash := BlockHasher{}.Hash(prevHeader)
+	if hash != b.PrevBlockHash {
+		return fmt.Errorf("invalid previous block hash")
+	}
+
 	if err := b.Verify(); err != nil {
 		return err
 	}
